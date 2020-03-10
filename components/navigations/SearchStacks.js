f9097d9b5cb15e4ffd4044e50cfb1181618c4f7e
dml1994 <dml94@live.com>
2020-03-10 19:42:59 +0100

    feat - Navigation Extend
    Añadida navegación interna para las búsquedas, además de las vistas
    de búsqueda de paseos y de alojamientos.

--- components/navigations/SearchStacks.js
+++ components/navigations/SearchStacks.js
@@ -1,11 +1,25 @@
 import { createStackNavigator } from "react-navigation-stack";
 import SearchScreen from "../screens/Search";
+import SearchWalksScreen from "../screens/SearchWalks";
+import SearchAccommodationsScreen from "../screens/SearchAccommodations";
 
 const SearchScreenStacks = createStackNavigator({
   Search: {
     screen: SearchScreen,
     navigationOptions: () => ({
       title: "Search"
+    })
+  },
+  SearchWalks: {
+    screen: SearchWalksScreen,
+    navigationOptions: () => ({
+      title: "Search Walks"
+    })
+  },
+  SearchAccommodations: {
+    screen: SearchAccommodationsScreen,
+    navigationOptions: () => ({
+      title: "Search Accommodations"
     })
   }
 });
