885c1141c24a4b2104851f413361feb8b72583d0
Sergio <sergiotb15@gmail.com>
2020-03-13 14:59:59 +0100

    feat - Nuevos datos añadido a la bbdd #5
    Añadidos 4 usuarios a la base de datos, dos administradores y dos wauwers, adaptados al nuevo modelo.

--- components/screens/Home.js
+++ components/screens/Home.js
@@ -1,196 +1,149 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 import { db } from "../population/config";
+import { TextInput } from "react-native";
 
 export default function Home() {
+
   const populteUsers = () => {
-    alert("Entra al populador");
-    let id = db.ref().child("wauwers").push().key;
-    var query = db.ref().child("wauwers/" + id);
+    /*
+    Admin users
+    */
+
+    let idRolAdmin = db.ref().child('rol').push().key;
+    let query = db.ref().child('rol/' + idRolAdmin);
+    query.set({
+      id: idRolAdmin,
+      nombre: 'administrador'
+    });
+
+    let idUAAdmin = db.ref().child('user_account').push().key;
+    query = db.ref().child('user_account/' + idUAAdmin);
+    query.set({
+      id: idUAAdmin,
+      username: 'usernameAdmin1',
+      //MD5 hash for password passwordAdmin1
+      password: '19f989b798aa3bb9bdb53394dceacf2c',
+      rol: [idRolAdmin]
+    });
+
+    let idAdmin = db.ref().child('administrator').push().key;
+    query = db.ref().child('administrator/' + idAdmin);
+    query.set({
+      id: idAdmin,
+      name: 'AdminName1',
+      surname: 'AdminSurname1',
+      dni: '11111111H',
+      photo: 'https://picsum.photos/',
+      avgScore: 7.27,
+      user_account: idUAAdmin
+    });
+    console.log("Popula al primer admin");
+
+    idRolAdmin = db.ref().child('rol').push().key;
+    query = db.ref().child('rol/' + idRolAdmin);
+    query.set({
+      id: idRolAdmin,
+      nombre: 'administrator'
+    });
+
+    idUAAdmin = db.ref().child('user_account').push().key;
+    query = db.ref().child('user_account/' + idUAAdmin);
+    query.set({
+      id: idUAAdmin,
+      username: 'usernameAdmin2',
+      //MD5 hash for password passwordAdmin2
+      password: '3e2d086cbf3854d53afdf5ae87db0f39',
+      rol: [idRolAdmin]
+    });
+
+    idAdmin = db.ref().child('administrator').push().key;
+    query = db.ref().child('administrator/' + idAdmin);
+    query.set({
+      id: idAdmin,
+      name: 'AdminName2',
+      surname: 'AdminSurname2',
+      dni: '12345678A',
+      photo: 'https://picsum.photos/',
+      avgScore: 4.96,
+      user_account: idUAAdmin
+    });
+
+    /* 
+    Wauwers users
+    */
+
+    let idRolWauwer = db.ref().child('rol').push().key;
+    query = db.ref().child('rol/' + idRolWauwer);
+
+    query.set({
+      id: idRolWauwer,
+      nombre: 'wauwer'
+    });
+
+    let idUAWauwer = db.ref().child('user_account').push().key;
+    query = db.ref().child('user_account/' + idUAWauwer);
+    query.set({
+      id: idUAWauwer,
+      username: 'usernameWauwer1',
+      //MD5 hash for password usernamePassword1
+      password: '2ccb842f36e0b9026649b91c756460a7',
+      rol: idRolWauwer
+    });
+
+
+
+    let idWauwer = db.ref().child("wauwers").push().key;
+    query = db.ref().child("wauwers/" + idWauwer);
     console.log("Popula el usuario", 1)
     query.set({
-      id: id,
-      email: "nombre" + 1 + "@nombre.com",
-      name: "Nombre " + 1,
-      surname: "Apellido " + 1,
-      admin: true,
-      pets: [
-        {
-          name: "Perro " + 1,
-          ppp: true,
-          breed: "Pastor alemán"
-        }
-      ]
+      id: idWauwer,
+      name: 'WauwerName1',
+      surname: 'WauwerSurname1',
+      dni: '11111112L',
+      photo: 'https://picsum.photos/',
+      avgScore: 7.25,
+      wauwPoints: 145,
+      paypalURL: 'www.paypal.com',
+      petNumber: 2,
+      description: 'Tengo dos perretes muy monos con ganas de ser paseados porque mi dueño tiene que realizar horas extras y trabajar los fines de semana en la empresa aunque eso es ilegal.'
     });
-    console.log("Popula el usuario", 2)
-    id = db.ref().child("wauwers").push().key;
-    query = db.ref().child("wauwers/" + id);
+
+    idRolWauwer = db.ref().child('rol').push().key;
+    query = db.ref().child('rol/' + idRolWauwer);
+    
     query.set({
-      id: id,
-      email: "nombre" + 2 + "@nombre.com",
-      name: "Nombre " + 2,
-      surname: "Apellido " + 2,
-      admin: false,
-      pets: [
-        {
-          name: "Perro " + 2,
-          birthDate: new Date(),
-          ppp: true,
-          breed: "Pastor alemán"
-        },
-        {
-          name: "Perro " + 2,
-          birthDate: new Date(),
-          ppp: false,
-          breed: "Yorkshire"
-        }
-      ]
+      id: idRolWauwer,
+      nombre: 'wauwer'
     });
-    console.log("Popula el usuario", 3)
-    id = db.ref().child("wauwers").push().key;
-    query = db.ref().child("wauwers/" + id);
+
+    idUAWauwer = db.ref().child('user_account').push().key;
+    query = db.ref().child('user_account/' + idUAWauwer);
+
     query.set({
-      id: id,
-      email: "nombre" + 3 + "@nombre.com",
-      name: "Nombre " + 3,
-      surname: "Apellido " + 3,
-      admin: false,
-      pets: [
-        {
-          name: "Perro " + 3,
-          birthDate: new Date(),
-          ppp: true,
-          breed: "Pastor alemán"
-        }
-      ]
+      id: idUAWauwer,
+      username: 'usernameWauwer2',
+      password: '6e1fc9f971967bea754a6d37d98484ee',
+      rol: idRolWauwer
     });
-    console.log("Popula el usuario", 4)
-    id = db.ref().child("wauwers").push().key;
-    query = db.ref().child("wauwers/" + id);
+
+    idWauwer = db.ref().child('wauwers').push().key;
+    query = db.ref().child('wauwers/' + idWauwer);
+
     query.set({
-      id: id,
-      email: "nombre" + 4 + "@nombre.com",
-      name: "Nombre " + 4,
-      surname: "Apellido " + 4,
-      admin: false,
-      pets: [
-        {
-          name: "Perro " + 4,
-          birthDate: new Date(),
-          ppp: true,
-          breed: "Pastor alemán"
-        }
-      ]
-    });
-    console.log("Popula el usuario", 5)
-    id = db.ref().child("wauwers").push().key;
-    query = db.ref().child("wauwers/" + id);
-    query.set({
-      id: id,
-      email: "nombre" + 5 + "@nombre.com",
-      name: "Nombre " + 5,
-      surname: "Apellido " + 5,
-      admin: false,
-      pets: [
-        {
-          name: "Perro " + 5,
-          birthDate: new Date(),
-          ppp: true,
-          breed: "Pastor alemán"
-        }
-      ]
-    });
-    console.log("Popula el usuario", 6)
-    id = db.ref().child("wauwers").push().key;
-    query = db.ref().child("wauwers/" + id);
-    query.set({
-      id: id,
-      email: "nombre" + 6 + "@nombre.com",
-      name: "Nombre " + 6,
-      surname: "Apellido " + 6,
-      admin: false,
-      pets: [
-        {
-          name: "Perro " + 6,
-          birthDate: new Date(),
-          ppp: true,
-          breed: "Pastor alemán"
-        }
-      ]
-    });
-    console.log("Popula el usuario", 7)
-    id = db.ref().child("wauwers").push().key;
-    query = db.ref().child("wauwers/" + id);
-    query.set({
-      id: id,
-      email: "nombre" + 7 + "@nombre.com",
-      name: "Nombre " + 7,
-      surname: "Apellido " + 7,
-      admin: false,
-      pets: [
-        {
-          name: "Perro " + 7,
-          birthDate: new Date(),
-          ppp: true,
-          breed: "Pastor alemán"
-        }
-      ]
-    });
-    console.log("Popula el usuario", 8)
-    id = db.ref().child("wauwers").push().key;
-    query = db.ref().child("wauwers/" + id);
-    query.set({
-      id: id,
-      email: "nombre" + 8 + "@nombre.com",
-      name: "Nombre " + 8,
-      surname: "Apellido " + 8,
-      admin: false,
-      pets: [
-        {
-          name: "Perro " + 8,
-          birthDate: new Date(),
-          ppp: true,
-          breed: "Pastor alemán"
-        }
-      ]
-    });
-    console.log("Popula el usuario", 9)
-    id = db.ref().child("wauwers").push().key;
-    query = db.ref().child("wauwers/" + id);
-    query.set({
-      id: id,
-      email: "nombre" + 9 + "@nombre.com",
-      name: "Nombre " + 9,
-      surname: "Apellido " + 9,
-      admin: false,
-      pets: [
-        {
-          name: "Perro " + 9,
-          birthDate: new Date(),
-          ppp: true,
-          breed: "Pastor alemán"
-        }
-      ]
-    });
-    console.log("Popula el usuario", 10)
-    id = db.ref().child("wauwers").push().key;
-    query = db.ref().child("wauwers/" + id);
-    query.set({
-      id: id,
-      email: "nombre" + 10 + "@nombre.com",
-      name: "Nombre " + 10,
-      surname: "Apellido " + 10,
-      admin: false,
-      pets: [
-        {
-          name: "Perro " + 10,
-          birthDate: new Date(),
-          ppp: true,
-          breed: "Pastor alemán"
-        }
-      ]
+      id: idWauwer,
+      name: 'Wauwer2',
+      surname: 'WauwerSurname2',
+      dni: '11122233A',
+      photo: 'https://picsum.photos/',
+      avgScore: 8.99,
+      wauwPoints: 100,
+      paypalURL: 'www.paypal.com',
+      petNumber: 1,
+      description: 'Torrente es un perro gordo, vago y salido. Pero cuando sale, se tira sobre el primer perro que ve'
     });
   };
+
   return (
     <View>
       <Text> Página del home</Text>
