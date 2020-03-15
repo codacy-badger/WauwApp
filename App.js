bf0be15659b387d77c33a919eb8dc62326382d1c
mardorcar <martadorantecardoso@gmail.com>
2020-03-15 19:57:06 +0100

    feat - Homescreen integrado

--- App.js
+++ App.js
@@ -4,7 +4,16 @@ import {  createAppContainer, createSwitchNavigator } from 'react-navigation';
 import LoginScreen from './components/screens/LoginScreen';
 import DashboardScreen from './components/screens/DashboardScreen';
 import LoadingScreen from './components/screens/LoadingScreen';
+import { YellowBox } from 'react-native';
+import _ from 'lodash';
 
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
 
 export default class App extends React.Component{
   render(){
--- components/navigations/HomeStack.js
+++ components/navigations/HomeStack.js
@@ -1,12 +1,19 @@
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "../screens/Home";
+import AnimalSheltersScreen from "../screens/AnimalShelters";
+
 
 const HomeScreenStack = createStackNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: () => ({
-      title: "Inicio",
-      headerTitleAlign: "center"
+      title: "Inicio"
+    })
+  },
+  AnimalShelters: {
+    screen: AnimalSheltersScreen,
+    navigationOptions: () => ({
+      title: "Protectoras"
     })
   }
 });
