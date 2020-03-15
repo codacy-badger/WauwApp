72180d783e0538cca90b92ed36bebcd5acc9e03e
mardorcar <martadorantecardoso@gmail.com>
2020-03-15 19:18:12 +0100

    feat - Login Google integrado

--- App.js
+++ App.js
@@ -1,6 +1,30 @@
-import React from "react";
-import Navigation from './components/navigations/Navigation';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native'; 
+import {  createAppContainer, createSwitchNavigator } from 'react-navigation';
+import LoginScreen from './components/screens/LoginScreen';
+import DashboardScreen from './components/screens/DashboardScreen';
+import LoadingScreen from './components/screens/LoadingScreen';
 
-export default function App() {
-  return <Navigation />;
+
+export default class App extends React.Component{
+  render(){
+    return <AppNavigator/>
+  }
 }
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen:LoadingScreen,
+  LoginScreen:LoginScreen,
+  DashboardScreen:DashboardScreen
+})
+
+const AppNavigator = createAppContainer(AppSwitchNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
