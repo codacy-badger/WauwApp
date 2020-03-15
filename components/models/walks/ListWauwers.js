373aabe2ca2d94fb93e5673bb86f78661da40217
Pedro <pedrosweng@gmail.com>
2020-03-15 17:39:25 +0100

    feat - styling and filtering in progress

--- components/models/walks/ListWauwers.js
+++ components/models/walks/ListWauwers.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, TouchableOpacity, SafeAreaView } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, SafeAreaView, StyleSheet } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import { Card, Image } from "react-native-elements";
 
 export default function ListWauwers(props){
     const {wauwerList}  = props;
@@ -29,16 +30,50 @@ function Wauwer(wauwer){
     console.log(wauwer);
     return (
         <TouchableOpacity
-            
+        onPress={() =>
+            navigation.navigate("Wauwer", {
+              wauwer: wauwer.wauwer
+            })
+          }
         >
             <View>
-                <View>
-                    <Text> {wauwer.wauwer.name} </Text>
-                    <Text> {wauwer.wauwer.id} </Text>
-                    <Text> ===================== </Text>
-                    
-                </View>
+                <Card style={styles.row}>
+                    <View style={styles.row}>
+                        <Image
+                            style={{width: 50, height: 50}}
+                            source={{uri: wauwer.wauwer.photo}}
+                        />
+                        <View style={styles.column_left}>
+                            <Text> {wauwer.wauwer.name} </Text>
+                            <Text> {wauwer.wauwer.avgScore} </Text>
+                        </View>
+                    </View>
+                </Card> 
             </View>
         </TouchableOpacity>
     );
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: 10
+        },
+    column: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: 20
+    },
+    column_left: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "space-between",
+        padding: 20
+    },
+});
--- components/screens/SearchWalks.js
+++ components/screens/SearchWalks.js
@@ -6,11 +6,27 @@ import { db } from "../population/config";
 
 export default function SearchWalks() {
   // QUERY --------------------------------------------------------------------
-  var wauwers = [];
+  /* var wauwers = [];
   db.ref().child('wauwers').orderByKey().on('child_added', (snap) => {
     wauwers.push(snap.val());
+  }); */
+
+  var usersIdAvailables = [];
+  db.ref()
+  .child('availability_wauwers')
+  .on('child_added', snap => {
+    usersIdAvailables.push(snap.val().idWauwer);
   });
 
+  var wauwersId = Array.from(new Set(usersIdAvailables));
+  var wauwers = [];
+  for (let i = 0; i < wauwersId.length; i++) {
+    db.ref().child('wauwers/' + wauwersId[i])
+    .on('value', snap => {
+      wauwers.push(snap.val());
+    });
+  }
+  console.log(wauwers);
   return (
    <View>
       <ListWauwers wauwerList = {wauwers}/>
