44ffb6078460fe4fff18725be9251015b94c3c39
dml1994 <dml94@live.com>
2020-03-19 20:34:36 +0100

    feat - Profile Buttons #28
    1. Creadas las vistas 'ProfileAddDogFrom', 'ProfileSitterForm'
    y 'ProfileWalkerForm'.
    2. Agrupadas las vistas del perfil en la carpeta 'Profile'.
    3. Insertados los 3 botones con navegación en la vista de perfil.
    4. Inserta imagen 'prints' como se parador de bloques.

--- components/navigations/ProfileStack.js
+++ components/navigations/ProfileStack.js
@@ -1,11 +1,35 @@
 import { createStackNavigator } from "react-navigation-stack";
-import ProfileScreen from "../screens/Profile";
+import ProfileScreen from "../screens/Profile/Profile";
+import ProfileWalkerFormScreen from "../screens/Profile/ProfileWalkerForm";
+import ProfileAddDogFormScreen from "../screens/Profile/ProfileAddDogForm";
+import ProfileSitterFormScreen from "../screens/Profile/ProfileSitterForm";
 
 const ProfileScreenStacks = createStackNavigator({
   Profile: {
     screen: ProfileScreen,
     navigationOptions: () => ({
       title: "Mi perfil",
+      headerTitleAlign: "center"
+    })
+  },
+  ProfileWalkerForm: {
+    screen: ProfileWalkerFormScreen,
+    navigationOptions: () => ({
+      title: "Datos de Paseador",
+      headerTitleAlign: "center"
+    })
+  },
+  ProfileSitterForm: {
+    screen: ProfileSitterFormScreen,
+    navigationOptions: () => ({
+      title: "Datos de Cuidador",
+      headerTitleAlign: "center"
+    })
+  },
+  ProfileAddDogForm: {
+    screen: ProfileAddDogFormScreen,
+    navigationOptions: () => ({
+      title: "Datos del Perro",
       headerTitleAlign: "center"
     })
   }
---
+++ components/screens/Profile/Profile.js
@@ -0,0 +1,138 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  SafeAreaView
+} from "react-native";
+import firebase from "firebase";
+import UserGuest from "../../account/UserGuest";
+import { Button } from "react-native-elements";
+import { YellowBox } from "react-native";
+import _ from "lodash";
+import { globalStyles } from "../../styles/global";
+import { withNavigation } from "react-navigation";
+
+YellowBox.ignoreWarnings(["Setting a timer"]);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf("Setting a timer") <= -1) {
+    _console.warn(message);
+  }
+};
+
+function Profile(props) {
+  const { navigation } = props;
+
+  return (
+    <SafeAreaView style={styles.area}>
+      <ScrollView scrollEventThrottle={16}>
+        <View style={styles.area}>
+          <View style={styles.viewStyle}>
+            <UserGuest />
+          </View>
+          <View style={styles.viewStyle2}>
+            <Image
+              source={require("../../../assets/images/prints.png")}
+              style={styles.ImageIconStyle}
+            />
+            <Button
+              buttonStyle={styles.btnStyle2}
+              containerStyle={styles.btnContainer2}
+              title="Añadir un Perro"
+              onPress={() => navigation.navigate("ProfileAddDogForm")}
+            />
+            <Button
+              buttonStyle={styles.btnStyle2}
+              containerStyle={styles.btnContainer2}
+              title="Quiero ser Cuidador"
+              onPress={() => navigation.navigate("ProfileSitterForm")}
+            />
+            <Button
+              buttonStyle={styles.btnStyle2}
+              containerStyle={styles.btnContainer2}
+              title="Quiero ser Paseador"
+              onPress={() => navigation.navigate("ProfileWalkerForm")}
+            />
+            <Image
+              source={require("../../../assets/images/prints.png")}
+              style={styles.ImageIconStyle}
+            />
+
+            <Button
+              buttonStyle={styles.btnStyle}
+              containerStyle={styles.btnContainer}
+              title="Cerrar sesión"
+              onPress={() => firebase.auth().signOut()}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default withNavigation(Profile);
+
+const styles = StyleSheet.create({
+  btnStyle: {
+    backgroundColor: "#ff7549",
+    borderRadius: 30,
+    marginTop: 5,
+    marginBottom: 5
+  },
+  btnContainer: {
+    alignItems: "center",
+    alignSelf: "center",
+    width: "90%",
+    backgroundColor: "#ff7549",
+    marginTop: 10,
+    marginRight: 20,
+    marginLeft: 20,
+    marginBottom: 50
+  },
+  btnStyle2: {
+    backgroundColor: "#443099",
+    borderRadius: 30,
+    marginTop: 5,
+    marginBottom: 5
+  },
+  btnContainer2: {
+    alignItems: "center",
+    alignSelf: "center",
+    width: "75%",
+    backgroundColor: "#443099",
+    marginTop: 5,
+    marginRight: 20,
+    marginLeft: 20,
+    marginBottom: 10
+  },
+  viewStyle: {
+    flex: 1,
+    paddingTop: 20,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  viewStyle2: {
+    flex: 1,
+    padding: 20,
+    paddingBottom: 60,
+    marginBottom: 20
+  },
+  ImageIconStyle: {
+    height: "20%",
+    width: "90%",
+    resizeMode: "stretch",
+    backgroundColor: "transparent",
+    alignSelf: "center",
+    marginBottom: 5
+  },
+  imageView: {
+    backgroundColor: "transparent"
+  },
+  area: {
+    flex: 1
+  }
+});
