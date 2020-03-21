183c1a6f150ed323ef910b0f470e293928a025f6
dml1994 <dml94@live.com>
2020-03-21 21:50:53 +0100

    feat - Location button
    Eliminado el formulario creado anteriormente y crada nueva vista

--- components/account/AccountOptions.js
+++ components/account/AccountOptions.js
@@ -5,7 +5,6 @@ import Modal from "./Modal";
 import ChangeNameForm from "./ChangeNameForm";
 import ChangeEmailForm from "./ChangeEmailForm";
 import ChangeDescriptionForm from "./ChangeDescriptionForm";
-import ChangeLocationForm from "./ChangeLocationForm";
 
 export default function AccountOptions(props) {
   const [isVisibleModal, setIsVisibleModal] = useState(false);
@@ -30,15 +29,6 @@ export default function AccountOptions(props) {
       iconNameRight: "chevron-right",
       iconColorRight: "#ccc",
       onPress: () => selectedComponent("description")
-    },
-    {
-      title: "Cambiar mi Localización",
-      iconType: "material-community",
-      iconNameLeft: "map-marker",
-      iconColorLeft: "#ccc",
-      iconNameRight: "chevron-right",
-      iconColorRight: "#ccc",
-      onPress: () => selectedComponent("location")
     }
   ];
 
@@ -58,15 +48,6 @@ export default function AccountOptions(props) {
       case "description":
         setRenderComponent(
           <ChangeDescriptionForm
-            id={userInfo.id}
-            desc={userInfo.description}
-            setIsVisibleModal={setIsVisibleModal}
-            setReloadData={setReloadData}
-          />
-        );
-      case "location":
-        setRenderComponent(
-          <ChangeLocationForm
             id={userInfo.id}
             desc={userInfo.description}
             setIsVisibleModal={setIsVisibleModal}
