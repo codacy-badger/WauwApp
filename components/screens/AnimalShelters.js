0770b45290e7ee2f2e6efffab12cf73ebc456fe8
mardorcar <martadorantecardoso@gmail.com>
2020-03-21 18:42:24 +0100

    feat - Listar mis solicitudes

--- components/screens/AnimalShelters.js
+++ components/screens/AnimalShelters.js
@@ -1,5 +1,18 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  SafeAreaView,
+  ActivityIndicator
+} from "react-native";
+import { Button, Avatar, Rating, Card, Image } from "react-native-elements";
+import { ScrollView } from "react-native-gesture-handler";
+import { db } from "../population/config.js";
+import { withNavigation } from "react-navigation";
+import { email } from "../account/QueriesProfile";
 import { YellowBox } from 'react-native';
 import _ from 'lodash';
 
@@ -11,14 +24,136 @@ console.warn = message => {
   }
 };
 
-export default function AnimalShelters() {
-  return (
-    <View>
-      <Text style = {{fontSize:24, fontWeight:'700', 
-      paddingHorizontal:20}}>
-          Página de información de protectoras
-      </Text>
+function myRequests(props) {
 
+  const { navigation } = props;
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [reloadData, setReloadData] = useState(false);
+
+  var requests = [];
+
+  var wauwerId = "";
+
+  db.ref("wauwers")
+    .orderByChild("email")
+    .equalTo(email)
+    .on("child_added", snap => {
+      wauwerId = snap.val().id;
+    });
+
+
+  db.ref("request")
+    .orderByChild("wauwerId")
+    .equalTo(wauwerId)
+    .on("child_added", snap => {
+      requests.push(snap.val());
+    });
+
+
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        {requests ? (
+          <FlatList
+            data={requests}
+            renderItem={request => (
+              <Request request={request.item} navigation={navigation} />
+            )}
+            keyExtractor={request => {
+              request.id;
+            }}
+          />
+        ) : (
+            <View>
+              <Text> No hay solicitudes </Text>
+            </View>
+          )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+function Request(requestIn) {
+  const { request, navigation } = requestIn;
+  return (
+    <View style={styles.separacion}>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("ShowRequest", {
+            request: request
+          })
+        }
+      >
+        <View style={styles.tarjeta}>
+          <View style={styles.row}>
+            <Image
+              style={{ width: 50, height: 50 }}
+              source={{ uri: request.accommodation.wauwer.photo }}
+            />
+            <View style={styles.column_left}>
+              <Text> {request.accommodation.wauwer.name} </Text>
+            </View>
+            <View style={styles.column_right}>
+              <Text> {request.price} €</Text>
+              <Text> {request.pending} </Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
     </View>
   );
+}
+
+export default withNavigation(myRequests);
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10
+  },
+  column: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20
+  },
+  column_left: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    padding: 20
+  },
+  column_right: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+    padding: 20
+  },
+  tarjeta: {
+    elevation: 1,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 25,
+    borderStyle: "solid"
+  },
+  separacion: {
+    paddingTop: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 5
+  },
+  loading: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
   }
+});
+
--- components/screens/Profile/ProfileSitterForm.js
+++ components/screens/Profile/ProfileSitterForm.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState , useEffect} from "react";
+import { StyleSheet, Text, TextInput, Button, View, Image } from "react-native";
+import { withNavigation } from "react-navigation";
 
 export default function ProfileSitterForm() {
+
   return (
     <View>
       <Text>Formulario de datos de cuidador</Text>
