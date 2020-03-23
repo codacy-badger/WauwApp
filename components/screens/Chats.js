d1bfdb89c692f3eecc5a71ff800a74435bf5c8ab
Alberto Guerrero <albergr1197@gmail.com>
2020-03-23 23:16:44 +0100

    fix - Implementación chat

--- components/screens/Chats.js
+++ components/screens/Chats.js
@@ -1,10 +1,31 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+import _ from 'lodash';
+import Chat from "../chat/Chat";
+import { email } from '../account/QueriesProfile';
+import { db } from "../population/config.js";
 
 export default function Chats() {
+  //const toastRef = useRef();
+  const [userInfo, setUserInfo] = useState([]);
+
+  useEffect(() => {
+    db.ref("wauwers")
+      .orderByChild("email")
+      .equalTo(email)
+      .on("value", function (snap) {
+        snap.forEach(function (child) {
+          setUserInfo(child.val());
+        });
+      });
+  }, []);
+
   return (
-    <View>
-      <Text>Vista de los Chats</Text>
-    </View>
+    <Chat user={{
+      name: userInfo.name,
+      _id: userInfo.id,
+      avatar: userInfo.photo
+    }}></Chat>
   );
 }
+
--- components/screens/Notifications.js
+++ components/screens/Notifications.js
@@ -1,13 +1,10 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef } from "react";
 import { View } from "react-native";
 import ListWalks from "./ListWalks";
 import Testeo from "../request/Testeo";
 import Toast from "react-native-easy-toast";
 import { YellowBox } from 'react-native';
 import _ from 'lodash';
-import Chat from "../chat/Chat";
-import { email } from '../account/QueriesProfile';
-import { db } from "../population/config.js";
 
 YellowBox.ignoreWarnings(['Setting a timer']);
 const _console = _.clone(console);
@@ -17,29 +14,13 @@ console.warn = message => {
   }
 };
 
-//<ListWalks toastRef={toastRef} />
-//<Toast ref={toastRef} position="center" opacity={0.7} />
-
-export default function Notifications() {
-  //const toastRef = useRef();
-  const [userInfo, setUserInfo] = useState([]);
-
-  useEffect(() => {
-    db.ref("wauwers")
-      .orderByChild("email")
-      .equalTo(email)
-      .on("value", function (snap) {
-        snap.forEach(function (child) {
-          setUserInfo(child.val());
-        });
-      });
-  }, []);
-
+export default function Chat() {
+  const toastRef = useRef();
   return (
-    <Chat user={{
-      name: userInfo.name,
-      _id: userInfo.id,
-      avatar: userInfo.photo
-    }}></Chat>
+    <View>
+      <ListWalks toastRef={toastRef} />
+      <Toast ref={toastRef} position="center" opacity={0.7} />
+    </View>
   );
 }
+
