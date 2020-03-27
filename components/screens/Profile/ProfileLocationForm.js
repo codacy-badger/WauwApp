52cb7efba3a77cd45df18a6966a7f76dafc87cc1
Carlos <carlosrb96@gmail.com>
2020-03-27 21:11:24 +0100

    fix-editar ubicacion falla algo

--- components/screens/Profile/ProfileLocationForm.js
+++ components/screens/Profile/ProfileLocationForm.js
@@ -3,19 +3,18 @@ import { Text, View, StyleSheet} from "react-native";
 import {Input, Button } from "react-native-elements";
 import { email } from "../../account/QueriesProfile"
 import {db} from "../../population/config";
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 export default function ProfileLocationForm(props) {
   const { navigation } = props;
   const[address, setAddress] = useState(null);
+  const[coordenadas,setCoordenadas] = useState({});
   const [reloadData, setReloadData] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [wauwer,setWauwer] = useState([]);
   
-  console.log(email);
-  console.log(wauwer.location);
-  console.log(reloadData);
-
 
   useEffect(() => {
     db.ref("wauwers")
@@ -28,6 +27,22 @@ export default function ProfileLocationForm(props) {
       });
     setReloadData(false);
   }, [reloadData]);
+
+
+  async function getLocationAsync() {
+      let currentAddress = await Location.geocodeAsync(address);
+      console.log(currentAddress);
+      
+      return currentAddress.coords;
+ }
+
+ async function reverseLocation(coordenadas) {
+   let direccion = await Location.reverseGeocodeAsync(coordenadas);
+   console.log(direccion);
+
+   return direccion.street;
+
+ }
 
   const updateLocation = () => {
     setError(null);
@@ -50,13 +65,36 @@ export default function ProfileLocationForm(props) {
         });
     }
   }
+
+  const ubicacionActual = () => { 
+    setIsLoading(true);
+    setCoordenadas(getLocationAsync());
+    setAddress(reverseLocation(coordenadas));
+    let userData = {
+      address: address,
+      coordenadas: coordenadas
+    }
+    db.ref("wauwers/" + wauwer.id)
+        .update(userData)
+        .then(() => {
+          setIsLoading(false);
+          setReloadData(true);
+        })
+        .catch(() => {
+          setError("Ha ocurrido un error");
+          setIsLoading(false);
+        });
+    }
+
+
+
   
   return (
         <View style={styles.view}>
           <Input
             placeholder="Avenida Reina Mercedes, Sevilla"
             containerStyle={styles.input}
-            defaultValue={wauwer.location}
+            defaultValue={wauwer.address}
             onChange={v => setAddress(v.nativeEvent.text)}
             errorMessage={error}
           />
@@ -64,20 +102,13 @@ export default function ProfileLocationForm(props) {
             title="Cambiar dirección"
             containerStyle={styles.btnContainer}
             buttonStyle={styles.btn}
-            onPress={updateLocation}
-            loading={isLoading}
-          />
-
-          <Button
-            title="Coger tu ubicación directamente"
-            containerStyle={styles.btnContainer}
-            buttonStyle={styles.btn}
-            onPress={() => navigation.navigate("ProfileAddLocationDefault")}
+            onPress={ubicacionActual}
             loading={isLoading}
           />
         </View>
   );
-}
+  }
+
 
 const styles = StyleSheet.create({
   paragraph: {
