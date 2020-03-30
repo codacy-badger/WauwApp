4d83b0bc704bd8249e0e041b05de46254a65e450
Pedro <pedrosweng@gmail.com>
2020-03-30 23:53:21 +0200

    test - fixing name of the test

--- components/__tests__/components/wauwapp.test.js
+++ components/__tests__/components/wauwapp.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import App from '../../../App';
 
 describe('<App />', () => {
-  it('testing the whola app of wauw', async () => {
+  it('testing the whole app of wauw', async () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree.children.length).toBe(1);
   });
